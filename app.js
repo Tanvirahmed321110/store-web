40dc48efe37d13a191794ed00cd14eb99d815066
tanvir321110 <tanvirahmed321110@gmail.com>
2024-01-18 07:42:34 +0600

    cart item 50% ok

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
-ECHO is on.
+
+
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const progressCircle = document.querySelector(".autoplay-progress svg");
+    const progressContent = document.querySelector(".autoplay-progress span");
+  
+    var swiper = new Swiper(".mySwiper", {
+      spaceBetween: 30,
+      centeredSlides: true,
+      autoplay: {
+        delay: 4500,
+        disableOnInteraction: false,
+      },
+      loop: true,
+      speed:1000,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      on: {
+        autoplayTimeLeft: function (s, time, progress) {
+          progressCircle.style.setProperty("--progress", 1 - progress);
+          progressContent.textContent = `${Math.ceil(time / 1000)}s`;
+        },
+      },
+    });
+  });
+  
